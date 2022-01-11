0910aaa0e3ff03e5ac355e2ca737213b47892b6f
Shion Ichikawa <shion1305@gmail.com>
2022-01-11 18:58:10 +0900

    Update 2164027_a1.c
    Updated Comment

--- 第12回/2164027_a1.c
+++ 第12回/2164027_a1.c
@@ -4,7 +4,7 @@
 //This is level up version of the answer.
 
 int main() {
-    //DO NOT USE SWITCH FOR SWITCHING ANSWER.
+    //NO NEED SWITCHING IN THIS ANSWER.
     int out[] = {1, 1, 2, 3, 5, 8, 13};
     for (int i = 0; i < n; ++i) {
         //CHANGE OUTPUT BY SELECTING NUM IN ARRAY out[] BY SURPLUS OF i
