d749bd50d11ef80f2d08c538c595bd555305fd0e
Shion Ichikawa <shion1305@gmail.com>
2022-01-27 20:09:11 +0900

    Emendment in 第14回/2164027_b3.c

--- 第14回/2164027_b3.c
+++ 第14回/2164027_b3.c
@@ -1,30 +1,5 @@
 #include <stdio.h>
 
-
-#define file "../data3.txt"
-
-typedef struct {
-    char name[400];
-    int score;
-} person;
-
-int main() {
-    int dSize;
-    //prepare file channel for the operation
-    FILE *fp = fopen(file, "r");
-    //acquire and repeat size of data
-    fscanf(fp, "%d", &dSize);
-    printf("人数: %d\n", dSize);
-    fprintf(fp, "%d", dSize);
-    person data[dSize];
-    for (int i = 0; i < dSize; ++i) {
-        fscanf(fp, "%s %d", &data[i].name, &data[i].score);
-    }
-    //show the ending message
-    printf("Your information has been recorded to %s", file);
-}#include <stdio.h>
-
-
 #define file "../data3.txt"
 
 int main() {
